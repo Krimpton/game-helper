3a589c7ab142a88622b25996eece0468a78a8c6c
Jessi <jessica.hoehne@dci-student.org>
2026-06-19 11:36:27 +0200

    Profile page added, header improved, CSS improved, profile picture upload added.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { searchGames } from "../services/gameService";
@@ -21,86 +20,77 @@ function Header({
   return (
     <header className="header">
 
-      {/* LOGO */}
-      <Link to="/" className="logo-link">
-        <h1 className="logo">GameHelper</h1>
-      </Link>
+  {/* LEFT: LOGO */}
+  <Link to="/" className="logo-link">
+    <h1 className="logo">GameHelper</h1>
+  </Link>
 
-      {/* NAVIGATION */}
-      <nav className="nav">
+  {/* RIGHT GROUP */}
+  <div className="header-right">
 
-        {/* GAMES */}
-        <div className="dropdown">
-          <button>Games ▼</button>
-
-          <div className="dropdown-content">
-            <Link to="/games">All Games</Link>
-            <Link to="/games/best">Best Ranking</Link>
-          </div>
+    <nav className="nav">
+      {/* GAMES */}
+      <div className="dropdown">
+        <button>Games ▼</button>
+        <div className="dropdown-content">
+          <Link to="/games">All Games</Link>
+          <Link to="/games/best">Best Ranking</Link>
         </div>
-
-        {/* CATEGORIES */}
-        <div className="dropdown">
-          <button>Categories ▼</button>
-
-          <div className="dropdown-content">
-            <Link to="/categories/action">Action</Link>
-            <Link to="/categories/rpg">RPG</Link>
-            <Link to="/categories/puzzle">Puzzle</Link>
-            <Link to="/categories/shooter">Shooter</Link>
-            <Link to="/categories/simulation">Simulator</Link>
-            <Link to="/categories/racing">Racing</Link>
-            <Link to="/categories/arcade">Arcade</Link>
-            <Link to="/categories/multiplayer">Multiplayer</Link>
-          </div>
-        </div>
-
-        {/* PLATFORMS */}
-        <div className="dropdown">
-          <button>Platforms ▼</button>
-
-          <div className="dropdown-content">
-            <Link to="/platforms/pc">PC</Link>
-            <Link to="/platforms/playstation">PlayStation</Link>
-            <Link to="/platforms/xbox">Xbox</Link>
-          </div>
-        </div>
-
-      </nav>
-
-      {/* SEARCH */}
-      <div className="search">
-        <input
-          type="text"
-          placeholder="Search games..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-
-        <button onClick={handleSearch}>
-          Search
-        </button>
       </div>
 
-      {/* USER */}
+      {/* CATEGORIES */}
+      <div className="dropdown">
+        <button>Categories ▼</button>
+        <div className="dropdown-content">
+          <Link to="/categories/action">Action</Link>
+          <Link to="/categories/rpg">RPG</Link>
+          <Link to="/categories/puzzle">Puzzle</Link>
+          <Link to="/categories/shooter">Shooter</Link>
+          <Link to="/categories/simulation">Simulator</Link>
+          <Link to="/categories/racing">Racing</Link>
+          <Link to="/categories/arcade">Arcade</Link>
+          <Link to="/categories/multiplayer">Multiplayer</Link>
+        </div>
+      </div>
+
+      {/* PLATFORMS */}
+      <div className="dropdown">
+        <button>Platforms ▼</button>
+        <div className="dropdown-content">
+          <Link to="/platforms/pc">PC</Link>
+          <Link to="/platforms/playstation">PlayStation</Link>
+          <Link to="/platforms/xbox">Xbox</Link>
+        </div>
+      </div>
+    </nav>
+
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search games..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+
+    {user && (
       <div className="user-section">
-        {user && (
-          <>
-            <span className="username">
-              👤 {user.username}
-            </span>
-
-            <button
-              className="logout-btn"
-              onClick={onLogout}
-            >
-              Logout
-            </button>
-          </>
-        )}
+        <Link to="/profile" className="user-link">
+          <img
+            src={
+              localStorage.getItem("profileImage") ||
+              "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+            }
+            className="header-avatar"
+          />
+          <span>{user.username}</span>
+        </Link>
       </div>
+    )}
 
-    </header>
+  </div>
+</header>
   );
 }
 
